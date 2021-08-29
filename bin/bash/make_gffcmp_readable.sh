f55a0825585d8251950da0b6baa9c363c890531a
LehmannN <20380914+LehmannN@users.noreply.github.com>
2021-08-29 14:57:26 +0200

    Update workflow references with violin plots

--- bin/bash/make_gffcmp_readable.sh
+++ bin/bash/make_gffcmp_readable.sh
@@ -1,13 +1,47 @@
 #!/usr/bin/env bash
 
-if [ -f gffcmp.stats ]; then
-	sed -n -e 5,6p -e 8,9p -e 11,27p gffcmp.stats | \
+# **************************************************************************** #
+# Define and check command line arguments
+# **************************************************************************** #
+usage() {
+	echo "Usage: $0
+	[-i gffcompare stats file (default: 'gffcmp.stats')]
+	[-o output directory (default: '.')]
+	"
+	1>&2; exit 1;
+}
+i='gffcmp.stats'
+o='.'
+while getopts ":o:i:" opt; do
+	case "${opt}" in
+		i)
+			i=${OPTARG} ;;
+		o)
+			o=${OPTARG} ;;
+		h)
+			usage ;;
+		*)
+			usage ;;
+	esac
+done
+shift $((OPTIND-1))
+if [ -z "$i" ] || [ -z "$o" ] ; then
+	usage
+fi
+
+# **************************************************************************** #
+# Make gffCompare stats file readable computationaly
+# **************************************************************************** #
+
+if [ -f $i ]; then
+	sed -n -e 5,6p -e 8,9p -e 11,27p $i | \
 	sed -e '1s/= Summary for dataset//g' | \
 	sed -e 's/#//g' -e '2,4s/ in//g' -e '2,4s/ loci//g' -e 's/multi-exon.*//g'| \
 	sed -e 's/|//g' -e 's/://g' -e 's/^ *//g' -e 's/   */\t/g' -e '/^$/d' -e '14,21s/\//\t/g' | \
-	tr -d '()' | \
+	tr -d '(%)' | \
 	tr -s '\t' '\t' | \
-	sed -e 's/\t */\t/g' -e 's/[[:blank:]]*$//' > gffcmp.stats.txt
+	sed -e 's/\t */\t/g' -e 's/[[:blank:]]*$//' > ${o}/${i}.txt
 else
-	echo "Error: file gffcmp.stats not found"
+	echo "Error: gffCompare stat file not found"
+	usage
 fi
